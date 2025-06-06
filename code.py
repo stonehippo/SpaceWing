fb83c4be8eb04408c4a3f603b83311f6a6f25563
George White <stonehippo@gmail.com>
2025-06-05 20:27:58 -0400

    Update to latest version

--- code.py
+++ code.py
@@ -1,10 +1,12 @@
 import time
 import board
 import busio
-import adafruit_bus_device
 import displayio
-from adafruit_displayio_ssd1306 import SSD1306
+import terminalio
 from adafruit_display_text import label
+from adafruit_displayio_ssd1306 import SSD1306
+
+# from adafruit_display_text import label
 from adafruit_bmp3xx import BMP3XX_I2C as BMP3XX
 from adafruit_lsm6ds.ism330dhcx import ISM330DHCX as LSM6DS
 from adafruit_lis3mdl import LIS3MDL
@@ -13,38 +15,92 @@ from adafruit_gps import GPS
 i2c = board.I2C()
 
 displayio.release_displays()
-display_bus = displayio.I2CDisplay(i2c, device_address=0x3c)
-display = SSD1306(display_bus, width=128, height=32) 				# set up the OLED display
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+display = SSD1306(display_bus, width=128, height=32)  # set up the OLED display
 
-bmp = BMP3XX(i2c)					# set up the pressure + temperature sensor
+bmp = BMP3XX(i2c)  # set up the pressure + temperature sensor
 
-accel_gyro = LSM6DS(i2c)			# set up the accelerometer + gyroscope
-mag = LIS3MDL(i2c)					# set up the magnetometer
+accel_gyro = LSM6DS(i2c)  # set up the accelerometer + gyroscope
+mag = LIS3MDL(i2c)  # set up the magnetometer
 
 uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
 
-gps = GPS(uart, debug=False)		# set up the GPS with I2C (rather than UART)
-gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")	#configure basic GGA and RMC info
-gps.send_command(b"PMTK220,1000")	# set GPS update rate to 1Hz
+gps = GPS(uart, debug=False)  # set up the GPS with I2C (rather than UART)
+gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+gps.send_command(b"PMTK220,1000")  # set GPS update rate to 1Hz
 
 # main loop
 last_print = time.monotonic()
-while True:
-	gps.update()
-	
-	current = time.monotonic()
-	if current - last_print > 1.0:	# set the display update to happen once per second or so
-		acceleration = accel_gyro.acceleration
-		gyro =accel_gyro.gyro
-		magnetic = mag.magnetic
 
-# uncomment to print to the console		
-# 		if not gps.has_fix:
-# 			print("Waiting for GPS fix...")
-# 			continue
-# 		print("Lat: {:.6f} Long: {:.6f} Qual: {:d}".format(gps.latitude, gps.longitude, gps.fix_quality))
-# 		print("Accel: X:{0:7.2f}, Y:{1:7.2f}, Z:{2:7.2f} m/s^2".format(*acceleration))
-# 		print("Gyro: X:{0:7.2f}, Y:{1:7.2f}, Z:{2:7.2f} rad/s".format(*gyro))
-# 		print("Mag: X:{0:7.2f}, Y:{1:7.2f}, Z:{2:7.2f} uT".format(*magnetic))
-# 		print("Pressure: {:6.4f} Temp: {:5.2f}".format(bmp.pressure, bmp.temperature))
-# 		print(" ")
+def prep_display():
+    display_group = displayio.Group()
+    color_bitmap = displayio.Bitmap(128, 32, 1)
+    color_palette = displayio.Palette(1)
+    color_palette[0] = 0x000000
+    bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+    display_group.append(bg_sprite)
+    return display_group
+
+def update_display_temp_pressure(temperature, pressure):
+    display_group = prep_display()
+    temp_text ="Temp: %0.1f F/ %0.1f C" % ((temperature * 1.8 + 32), temperature)
+    pressure_text = "Pressure: %0.1f kPa" % pressure
+
+    temp_lbl = label.Label(terminalio.FONT, text = temp_text, color=0xFFFFFF, x=1, y=8)
+    pressure_lbl = label.Label(terminalio.FONT, text = pressure_text, color=0xFFFFFF, x=1, y=22)
+
+    display_group.append(temp_lbl)
+    display_group.append(pressure_lbl)
+    display.root_group = display_group
+
+def update_display_location(longitude, latitude):
+    display_group = prep_display()
+    longitude_text = "Long: {:.6f}".format(gps.longitude)
+    latitude_text = "Lat: {:.6f}".format(latitude)
+
+    longitude_lbl = label.Label(terminalio.FONT, text=longitude_text, color=0xFFFFFF, x=1, y=8)
+    latitude_lbl = label.Label(terminalio.FONT, text=latitude_text, color=0xFFFFFF, x=1, y=22)
+
+    display_group.append(longitude_lbl)
+    display_group.append(latitude_lbl)
+    display.root_group = display_group
+
+def update_no_gps_fix():
+    display_group = prep_display()
+
+    lbl = label.Label(terminalio.FONT, text="Waiting for GPS fix...", color=0xFFFFFF, x=1, y=8)
+
+    display_group.append(lbl)
+    display.root_group = display_group
+
+# overall time for updating
+update_interval = 8.0
+
+# To-do
+# - read the state of the buttons
+# - set display and update mode based on buttons
+# - use GPS RTC to display time
+# - animate compass
+
+while True:
+    gps.update()
+
+    current = time.monotonic()
+    # set the display update to happen once per second or so
+    if current - last_print > update_interval:
+        acceleration = accel_gyro.acceleration
+        gyro = accel_gyro.gyro
+        magnetic = mag.magnetic
+        last_print = current
+
+        update_display_temp_pressure(bmp.temperature, bmp.pressure)
+        time.sleep(update_interval/2.0)
+        if gps.has_fix:
+            update_display_location(gps.longitude, gps.latitude)
+        else:
+            update_no_gps_fix()
+        # uncomment to print to the console
+        #print("Accel: X:{0:7.2f}, Y:{1:7.2f}, Z:{2:7.2f} m/s^2".format(*acceleration))
+        #print("Gyro: X:{0:7.2f}, Y:{1:7.2f}, Z:{2:7.2f} rad/s".format(*gyro))
+        #print("Mag: X:{0:7.2f}, Y:{1:7.2f}, Z:{2:7.2f} uT".format(*magnetic))
+        #print(" ")
---
+++ send_accel_to_bfc.py
@@ -0,0 +1,48 @@
+import time
+import board
+
+from adafruit_ble import BLERadio
+
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
+
+from adafruit_lsm6ds.ism330dhcx import ISM330DHCX as LSM6DS
+
+# set up the accelerometer + gyroscope
+i2c = board.I2C()
+accel_gyro = LSM6DS(i2c)
+
+# set up the radio hardware
+ble = BLERadio()
+
+# By default, your device will have some name like CIRCUITPYxxxx; let's make that more user friendly
+ble.name = "MyCPDevice"
+
+# set up the UART service. This virtual serial port lets you send text over BLE.
+uart = UARTService()
+
+# set up advertising, so your phone or PC will know that UART service is available on your device
+advertisement = ProvideServicesAdvertisement(uart)
+
+while True:
+  print("Advertising BLE services")
+  # start advertising
+  ble.start_advertising(advertisement)
+  # keep going until we get a connection
+  while not ble.connected:
+    pass
+    
+  # if we got here, we have a connection. Stop advertising!
+  ble.stop_advertising()
+  print("BLE connected")
+  
+  # do some work as long as we're connected
+  while ble.connected:
+    x, y, z = accel_gyro.acceleration
+    plot_data = f"{x}, {y}, {z}\n"
+    uart.write(plot_data.encode("utf-b"))
+    time.sleep(0.025)
+  
+  # we no longer have a connection, so we'll go back to the top of the loop
+  # and start advertising again
+  print("BLE disconnected")
